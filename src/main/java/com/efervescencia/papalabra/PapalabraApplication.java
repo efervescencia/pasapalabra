f5182eadc57c7ce5a548cc179c42484e3e5350cf
Julio Montero <juliointelek@hotmail.com>
2024-03-16 09:07:05 +0200

    commit inicial, login memory ok

---
+++ src/main/java/com/efervescencia/papalabra/PapalabraApplication.java
@@ -0,0 +1,13 @@
+package com.efervescencia.papalabra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PapalabraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PapalabraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/efervescencia/papalabra/PapalabraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.efervescencia.papalabra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PapalabraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
